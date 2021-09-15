e29b4ec02c70ed9361e87ee18b2cc9f3aca11426
Your Name <you@example.com>
2021-09-15 14:30:47 +0200

    done

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+
+export const fizzbuzz=(number)=>{
+    
+    if(!(number%3) && !(number%5)){
+        return 'FizzBuzz';
+    }else if(!(number%3)){
+
+        return 'Fizz';
+    }else if(!(number%5)){
+       
+        return 'Buzz';
+    }else{
+    
+        return number;
+    }
+
+}
